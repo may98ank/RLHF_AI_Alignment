e467488f1864785152562f8961f5a83a8d3257a2
may98ank <mayankgupta978@gmail.com>
2026-03-30 20:43:16 -0400

    Part 2: reward network and pairwise preference training (reward.params)

--- offline_reward_learning.py
+++ offline_reward_learning.py
@@ -1,11 +1,7 @@
 import gymnasium as gym
-import pygame
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import time
 import numpy as np
-import random
 from rollout_policy import generate_rollout
 from utils import mlp, Net
 
@@ -21,12 +17,11 @@ def generate_novice_demos(env):
     n_acts = env.action_space.n
     hidden_sizes=[32]
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
 
 
     demonstrations = []
     demo_returns = []
-    
+
     for checkpoint in checkpoints:
 
         policy = mlp(sizes=[obs_dim]+hidden_sizes+[n_acts],activation=nn.Tanh)
@@ -35,7 +30,7 @@ def generate_novice_demos(env):
         print("traj ground-truth return", ret)
         demonstrations.append(traj)
         demo_returns.append(ret)
-         
+
     return demonstrations, demo_returns
 
 
@@ -48,78 +43,74 @@ def create_training_data(trajectories, cum_returns, num_pairs):
     for n in range(num_pairs):
         ti = 0
         tj = 0
-        #pick two different trajectories 
+        #pick two different trajectories
         while(ti == tj):
             ti = np.random.randint(num_trajs)
             tj = np.random.randint(num_trajs)
         traj_i = trajectories[ti]
         traj_j = trajectories[tj]
-        
-	# Label based on cumulative returns 
+
+        # Label based on cumulative returns
         if cum_returns[ti] > cum_returns[tj]:
             label = 0
         else:
             label = 1
-        # print(cum_returns[ti], cum_returns[tj])
-        # print(label)
-        
+
         training_pairs.append((traj_i, traj_j))
         training_labels.append(label)
-       
+
     return training_pairs, training_labels
 
 
-# NOTE: This is a function for debugging. Do not use it to compute returns when you implement learn_reward(). 
+# NOTE: This is a function for debugging. Do not use it to compute returns when you implement learn_reward().
 # Instead, use Net.predict_return() so that you get back a tensor that does the bookeeping for autograd
 def predict_traj_return(net, traj):
     traj = np.array(traj)
+    device = next(net.parameters()).device
     traj = torch.from_numpy(traj).float().to(device)
     return net.predict_return(traj).item()
-    
+
 
 # Train the network
 def learn_reward(reward_network, optimizer, training_inputs, training_outputs, num_iter, checkpoint_dir):
-    #check if gpu available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-    #We will use a cross entropy loss for pairwise preference learning
+
     loss_criterion = nn.CrossEntropyLoss()
-    
-    #TODO: train reward function using the training data for the specified number of epochs (num_iter).
-    # training_inputs gives you a list of pairs of trajectories
-    # training_outputs gives you a list of labels (0 if first trajectory better, 1 if second is better)
-    # Hint: For simplicity, don't try to batch multiple preferences; just compute loss and update parameters for each preference separately
-    # Hint: Check the pytorch documentation for details on the tensor dimensions it expects
-    # Hint: You may find torch.cat useful.
-    # Hint: You may also find unsqueeze useful. For example, if x is the tensor [1,2,3] (which has shape [3]), then x.unsqueeze(0) returns [[1,2,3]] (which has shape [1,3])
 
+    reward_network.train()
+    for _ in range(num_iter):
+        for (traj_i, traj_j), label in zip(training_inputs, training_outputs):
+            ti = torch.tensor(np.asarray(traj_i), dtype=torch.float32, device=device)
+            tj = torch.tensor(np.asarray(traj_j), dtype=torch.float32, device=device)
+            logit_i = reward_network.predict_return(ti)
+            logit_j = reward_network.predict_return(tj)
+            logits = torch.stack([logit_i, logit_j]).unsqueeze(0)
+            target = torch.tensor([label], dtype=torch.long, device=device)
+            loss = loss_criterion(logits, target)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-
-
-    #After training we save the reward function weights    
     print("checkpointing")
-    torch.save(reward_net.state_dict(), checkpoint_dir)
+    torch.save(reward_network.state_dict(), checkpoint_dir)
     print("finished training")
 
 
 if __name__=="__main__":
 
     env = gym.make("CartPole-v0")
-    
+
     num_pairs = 20
     #create synthetic trajectories for RLHF
     trajectories, traj_returns = generate_novice_demos(env)
 
     #create pairwise preference data using ground-truth reward
     traj_pairs, traj_labels = create_training_data(trajectories, traj_returns, num_pairs)
-    
-    #TODO: hyper parameters that you may want to tweak or change
+
     num_iter = 100
     lr = 0.001
     checkpoint = "./reward.params" #where to save your reward function weights
 
-    # Now we create a reward network and optimize it using the training data.
-    #TODO: You will need to code up Net in utils.py   
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     reward_net = Net()
     reward_net.to(device)
@@ -127,7 +118,6 @@ if __name__=="__main__":
 
     import torch.optim as optim
     optimizer = optim.Adam(reward_net.parameters(),  lr=lr)
-    #TODO: You will need to implement learn_reward, you can add arguments or do whatever you want
     learn_reward(reward_net, optimizer, traj_pairs, traj_labels, num_iter, checkpoint)
 
 
@@ -142,4 +132,3 @@ if __name__=="__main__":
             print("A should be better\n")
         else:
             print("B should be better\n")
-
--- utils.py
+++ utils.py
@@ -1,15 +1,12 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.distributions.categorical import Categorical
-from torch.optim import Adam
 import numpy as np
 
 
 # Helper function to construct a feedforward multilayer perception that you can use in class Net if you want.
 # Do NOT modify, as this is used to construct the policy network.
 # Sizes is a list of the number of neurons in each layer (where the number of layers is the len of the list)
-# nn.Sequential allows you to skip explicitly defining a forward pass like you had to do in assignment 1. 
+# nn.Sequential allows you to skip explicitly defining a forward pass like you had to do in assignment 1.
 # See pyTorch documentation if you're confused (or feel free to define the network and forward() by hand as in assignment 1).
 def mlp(sizes, activation=nn.ReLU, output_activation=nn.Identity):
     # Build a feedforward neural network for the policy
@@ -21,25 +18,21 @@ def mlp(sizes, activation=nn.ReLU, output_activation=nn.Identity):
 
 
 class Net(nn.Module):
-    ##TODO Define the reward network architecture and functionality
-    # Be sure to implement both __init__() and predict_reward()
+    """Reward model: scalar reward per state; trajectory return is sum over timesteps."""
 
     def __init__(self):
         super().__init__()
-        #TODO define network architecture. 
-        # Feel free to use the mlp helper function above or construct the network and forward() manually like you did in assignment 1.
-        # Hint: states in cartpole are 4-dimensional (x,xdot,theta,thetadot)
-        # https://www.gymlibrary.dev/environments/classic_control/cart_pole/
-   
+        self.net = mlp(sizes=[4, 32, 32, 1], activation=nn.ReLU)
+
+    def forward(self, obs):
+        return self.net(obs).squeeze(-1)
 
     def predict_return(self, traj):
         '''calculate return (cumulative reward) of a trajectory (could be any number of timesteps)'''
-        #TODO should take in a trajectory and output a scalar cumulative reward estimate
-        
-
-
-
-
-	
-
-   
+        if not isinstance(traj, torch.Tensor):
+            traj = torch.as_tensor(traj, dtype=torch.float32, device=next(self.parameters()).device)
+        else:
+            traj = traj.float().to(next(self.parameters()).device)
+        if traj.dim() == 1:
+            traj = traj.unsqueeze(0)
+        return self.forward(traj).sum()
